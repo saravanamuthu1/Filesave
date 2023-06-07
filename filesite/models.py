ec27612494b5ede9c88139eb0cd720c665eb0a08
smuthusamy <saravanamuthu.muthusamy@slu.edu>
2023-06-06 19:19:03 -0500

    implemented sign up page

--- filesite/models.py
+++ filesite/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# add user class
 class User(models.Model):
     userName = models.CharField(max_length=50)
     password=models.CharField(max_length=50)
     def __str__(self):
         return self.user
-    
+# add post clas     
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     file_field = models.FileField(upload_to='uploads/')
     desc = models.TextField()
-    
+
     def __str__(self):
         return f'{self.user}=> {self.title}'
--- filesite/views.py
+++ filesite/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from .models import User, Post
+from django.http import HttpResponse
+from .models import User
+from django.contrib import messages
 
-# Create your views here.
+class Homeview(ListView):
+    def get(self,request):
+        return render(request,'main/base.html')
+    def post(self,request):
+        if request.method == 'POST':
+            usernamepage=request.POST.get('uname')
+            password1=request.POST.get('pwd1')
+            password2=request.POST.get('pwd2')
+            print(usernamepage)
+            print(password1)
+            print(password2)
+            if password1 == password2:
+                dataObject=User(userName=usernamepage,password=password1)
+                dataObject.save()
+                messages.success(request,'you have successfull created your account')
+                return redirect('homepage')
+            else:
+                messages.warning(request,'password are not same')
+                return redirect('homepage')
+
+
+class Signupview(ListView):
+    def get(self,request):
+        return render(request,'main/signup.html')
